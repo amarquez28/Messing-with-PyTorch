00e05808b70ca6f5e633b251d8308797e81e474b
carti69 <zhukow28@gmail.com>
2025-06-18 22:48:49 -0600

    added link and some more code

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#https://pytorch.org/get-started/locally/ 
+#link to install locally
 import torch
 import numpy as np
 
@@ -10,6 +12,7 @@ import numpy as np
 
 
 shape = (2,3)
+
 rand_tensor = torch.rand(shape)
 ones_tensor = torch.ones(shape)
 zeroes_tensor = torch.zeros(shape)
@@ -31,3 +34,18 @@ if torch.accelerator.is_available():
     tensor = tensor.to(torch.accelerator.current_accelerator())
 print(f"Device tensor is stored on: {tensor.device}")
 
+# This computes the matrix multiplication between two tensors. y1, y2, y3 will have the same value
+# ``tensor.T`` returns the transpose of a tensor
+y1 = tensor @ tensor.T
+y2 = tensor.matmul(tensor.T)
+
+y3 = torch.rand_like(y1)
+torch.matmul(tensor, tensor.T, out=y3)
+
+
+# This computes the element-wise product. z1, z2, z3 will have the same value
+z1 = tensor * tensor
+z2 = tensor.mul(tensor)
+
+z3 = torch.rand_like(tensor)
+torch.mul(tensor, tensor, out=z3)
